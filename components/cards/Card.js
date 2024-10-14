2e89e8275bf4b561d6d1e49f40c800eb1e34d48d
wxy0415 <qkrtkd727@gmail.com>
2024-10-14 16:07:56 +0900

    feat: card 컴포넌트 임시완성했습니다

---
+++ components/cards/Card.js
@@ -0,0 +1,47 @@
+import styles from "@/components/cards/Card.module.css";
+import Image from "next/image";
+
+export default function Card() {
+  return (
+    <>
+      <div className={styles["card-container"]}>
+        <Image
+          src="/card-default-img.svg"
+          className={styles["card-image"]}
+          width={360}
+          height={270}
+          alt="card-image"
+          priority
+        />
+        <div className={styles["card-information"]}>
+          <p className={styles["card-title"]}>title</p>
+          <div className={styles["card-information-wrapper"]}>
+            <div className={styles["card-information-grade-wrapper"]}>
+              <p className={styles["card-grade"]}>grade</p>
+              <p className={styles["card-grade-divider"]}>|</p>
+              <p className={styles["card-category"]}>category</p>
+            </div>
+            <p className={styles["card-nickname"]}>nickname</p>
+          </div>
+        </div>
+        <div className={styles["card-price-wrapper"]}>
+          <p className={styles["card-price-tag"]}>가격</p>
+          <p className={styles["card-price"]}>n P</p>
+        </div>
+        <div className={styles["card-stock-wrapper"]}>
+          <p className={styles["card-stock-tag"]}>잔여</p>
+          <p className={styles["card-stock"]}>
+            n <span className={styles["card-stock-denominator"]}>/ 5</span>
+          </p>
+        </div>
+        <Image
+          src="/logo.svg"
+          width={99.25}
+          height={18}
+          alt="logo"
+          className={styles["card-logo"]}
+        />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import styles from "@/styles/Home.module.css";
+import Card from "@/components/cards/Card.js";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Card />
+    </>
+  );
 }
