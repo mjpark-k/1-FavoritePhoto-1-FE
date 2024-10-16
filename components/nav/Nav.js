3dab8e851b76039e51edb9b0b6a59e93f0876497
mjpark <pmj9498@gmail.com>
2024-10-16 13:07:12 +0900

    feat: Nav UserDrop 기능 추가 UserDrop 컴포넌트로 닉네임 밑에 포지셔닝되게 구현 참고: 미디어 쿼리까지 구현하여 모바일일 때, 닉네임 밑이 아닌 왼쪽에 모달처럼 표시함 / 이것도 데이터 받을 때 수정할 것 같습니다.

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -12,6 +12,11 @@ import NonLogin from "./NonLogin";
 export default function Nav() {
   const router = useRouter();
   const [isLogin, setIsLogin] = useState(false);
+  const [userDrop, setUserDrop] = useState(false);
+
+  const handleUserDrop = () => {
+    setUserDrop(!userDrop);
+  };
 
   const login = () => {
     setIsLogin(true);
@@ -24,20 +29,31 @@ export default function Nav() {
   return (
     // 로그인과 회원가입 부분에 Nav가 안보이게 하기 위해
     // pathname을 이용하여 다른 style이 적용되게 했습니다.
-    <header
-      className={`${styles["nav"]} ${
-        router.pathname === "/login" || router.pathname === "/signup"
-          ? styles["nav-none"]
-          : ""
-      }`}
-    >
-      <div className={styles["container"]}>
-        <Image src={listIcon} className={styles["list"]} alt="list-icon" />
-        <Link href="/">
-          <Image src={mainLogo} className={styles["logo"]} alt="logo" />
-        </Link>
-        {isLogin ? <Loggedin logout={logout} /> : <NonLogin login={login} />}
-      </div>
-    </header>
+    <div>
+      <header
+        className={`${styles["nav"]} ${
+          router.pathname === "/login" || router.pathname === "/signup"
+            ? styles["nav-none"]
+            : ""
+        }`}
+      >
+        <div className={styles["container"]}>
+          <Image src={listIcon} className={styles["list"]} onClick={handleUserDrop} alt="list-icon" />
+          <Link href="/">
+            <Image src={mainLogo} className={styles["logo"]} alt="logo" />
+          </Link>
+          {isLogin ? (
+            <Loggedin
+              onClick={handleUserDrop}
+              logout={logout}
+              userDrop={userDrop}
+              setUserDrop={setUserDrop}
+            />
+          ) : (
+            <NonLogin login={login} />
+          )}
+        </div>
+      </header>
+    </div>
   );
 }
