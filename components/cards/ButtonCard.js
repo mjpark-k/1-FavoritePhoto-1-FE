704306e6b4e4bd51d4b6f2faa3f3c4d3f11823cf
mjpark <pmj9498@gmail.com>
2024-10-18 11:05:05 +0900

    feat: ButtonCard, EditExchangeInfo, CardEdit 컴포넌트 기능 추가 ButtonCard, EditExchangeInfo, CardEdit 컴포넌트를 추가하여 seller/photocard/[id] 페이지 완성 참고: 데이터 연동 후 받아오는 작업 이후에 약간의 수정이 있을 것 같습니다.

--- components/cards/ButtonCard.js
+++ components/cards/ButtonCard.js
@@ -4,6 +4,8 @@ import GradeCategory from "./info/GradeCategory";
 import Button from "../buttons/Button";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
+import ModalContainer from "../modal/ModalContainer";
+import ExchangeAuth from "../modal/contents/ExchangeAuth";
 
 /**
  *
@@ -13,11 +15,12 @@ import { useEffect, useState } from "react";
  * ex)
  * <ButtonCard style={"refuse-approval"}/>
  * <ButtonCard style={"cancel"}/>
- * 
+ *
  */
 
 export default function ButtonCard({ style }) {
   const [isMobile, setIsMobile] = useState(false); // 모바일 뷰 감지 상태
+  const [exchangeAuth, setExchangeAuth] = useState(null);
 
   // 창 크기를 감지하여 모바일인지 확인
   useEffect(() => {
@@ -33,6 +36,18 @@ export default function ButtonCard({ style }) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const isDecline = () => {
+    setExchangeAuth("거절");
+  };
+
+  const isApproval = () => {
+    setExchangeAuth("승인");
+  };
+
+  const isModalClose = () => {
+    setExchangeAuth(null);
+  };
+
   const buttonContainerClass = classNames({
     [styles[style]]: style,
   });
@@ -41,41 +56,49 @@ export default function ButtonCard({ style }) {
   const buttonChoice = style === "refuse-approval";
 
   return (
-    <div className={styles["card-container"]}>
-      <div className={styles["card-info-container"]}>
-        <Image
-          src="/card-default-img.svg"
-          className={styles["card-image"]}
-          width={360}
-          height={270}
-          alt="card-image"
-          priority
-        />
-        <div className={styles["card-information"]}>
-          <p className={styles["card-title"]}>title</p>
-          <div className={styles["card-information-wrapper"]}>
-            <GradeCategory style={"small"} />
-            <p className={styles["card-nickname"]}>nickname</p>
+    <>
+      <div className={styles["card-container"]}>
+        <div className={styles["card-info-container"]}>
+          <Image
+            src="/card-default-img.svg"
+            className={styles["card-image"]}
+            width={360}
+            height={270}
+            alt="card-image"
+            priority
+          />
+          <div className={styles["card-information"]}>
+            <p className={styles["card-title"]}>title</p>
+            <div className={styles["card-information-wrapper"]}>
+              <GradeCategory style={"small"} />
+              <p className={styles["card-nickname"]}>nickname</p>
+            </div>
+          </div>
+          <div className={styles["card-content-container"]}>
+            <div>content about photo</div>
           </div>
         </div>
-        <div className={styles["card-content-container"]}>
-          <div>content about photo</div>
+        <div className={buttonContainerClass}>
+          {buttonChoice ? (
+            <>
+              <Button style={"thin-gray-170px"} onClick={isDecline}>
+                {isMobile ? "거절" : "거절하기"}
+              </Button>
+              <Button style={"thin-main-170px"} onClick={isApproval}>
+                {isMobile ? "승인" : "승인하기"}
+              </Button>
+            </>
+          ) : (
+            <Button style={"thin-gray-360px"} children={"취소하기"} />
+          )}
         </div>
       </div>
-      <div className={buttonContainerClass}>
-        {buttonChoice ? (
-          <>
-            <Button style={"thin-gray-170px"}>
-              {isMobile ? "거절" : "거절하기"}
-            </Button>
-            <Button style={"thin-main-170px"}>
-              {isMobile ? "승인" : "승인하기"}
-            </Button>
-          </>
-        ) : (
-          <Button style={"thin-gray-360px"} children={"취소하기"} />
-        )}
-      </div>
-    </div>
+      {exchangeAuth && (
+        <ModalContainer
+          children={<ExchangeAuth exchangeAuth={exchangeAuth} />}
+          onClick={isModalClose}
+        />
+      )}
+    </>
   );
 }
--- components/cards/EditExchangeInfo.js
+++ components/cards/EditExchangeInfo.js
@@ -4,7 +4,7 @@ import editIcon from "@/public/edit-icon.svg";
 import GradeCategory from "./info/GradeCategory";
 import Button from "../buttons/Button";
 
-export default function EditExchangeInfo() {
+export default function EditExchangeInfo({ editModalClick }) {
   return (
     <div className={styles["container"]}>
       <div className={styles["title"]}>
@@ -14,8 +14,13 @@ export default function EditExchangeInfo() {
       <GradeCategory style={"medium"} />
       <div className={styles["content"]}>content about exchange request</div>
       <div className={styles["button-container"]}>
-        <Button children={"수정하기"} style={"thick-main-440px"} />
-        <Button children={"판매 내리기"} style={"thick-black-440px"} />
+        <Button
+          children={"수정하기"}
+          style={"thick-main-440px"}
+          onClick={editModalClick}
+        />
+        {/* delete 카드 판매 */}
+        <Button children={"판매 내리기"} style={"thick-black-440px-80px"} />
       </div>
     </div>
   );
