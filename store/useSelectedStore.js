77132479a8a9b1ffd776f908dbadd14a62dc3771
JM <167840750+mini-1018@users.noreply.github.com>
2024-10-18 14:27:31 +0900

    Revert "feat: 포토 생성페이지, 마이갤러리 상세페이지 구현했습니다"

--- store/useSelectedStore.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-export const useSelectedStore = create((set) => ({
-  selectedCard: null,
-  setSelectedCard: (card) => set({ selectedCard: card }),
-  clearSelectedCard: () => set({ selectedCard: null }),
-}));
-
-export default useSelectedStore;
