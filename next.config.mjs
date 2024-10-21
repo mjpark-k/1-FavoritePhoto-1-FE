c5ea05d4a5fd94f1b177342bf6f151b229828d6d
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-21 20:40:38 +0900

    feat : add rewrites setting
    쿠키를 받아오기 위해 백엔드에 동일한 도메인으로 요청하는 것처럼 보이게 rewrites 설정 추가
    
    주의 : .env에서 받아오는 BE baseUrl을 http://localhost:3000/api 로 수정해야 함

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://one-favoritephoto-1-be.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
