a9646dc8d17766454f2d322ea20527f8abdb811b
wxy0415 <qkrtkd727@gmail.com>
2024-10-22 16:21:57 +0900

    feat: 누락된  useMutation 추가됐습니다

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -7,7 +8,6 @@ const nextConfig = {
         hostname: "**",
       },
     ],
-    reactStrictMode: true,
   },
 };
 
