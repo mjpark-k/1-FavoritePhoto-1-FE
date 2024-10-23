0620c1f51b1669ff26e31b481935a507d20b396e
mini-1018 <anjaemin1018@gmail.com>
2024-10-23 11:18:01 +0900

    feat : 등급,카테고리 컴포넌트 api 연결

--- components/cards/info/GradeCategory.jsx
+++ components/cards/info/GradeCategory.jsx
@@ -5,16 +5,47 @@ import classNames from 'classnames';
  * @param style
  * 1. small
  * 2. medium
+ * @param grade
+ * 0-COMMON, 1-RARE, 2-SUPER RARE, 3-LEGENDARY, default-등급정보없음
+ * @param genre
+ * 0-풍경, 1-인물, 2-동물, 3-정물, 4-추상, default-기타
  */
-export default function GradeCategory({ style }) {
+export default function GradeCategory({ style, grade, genre }) {
   const textStyle = classNames({
     [styles[style]]: style,
   });
+
+  const gradeText =
+    grade === 0
+      ? 'COMMON'
+      : grade === 1
+      ? 'RARE'
+      : grade === 2
+      ? 'SUPER RARE'
+      : grade === 3
+      ? 'LEGENDARY'
+      : '등급정보없음';
+
+  const genreText =
+    genre === 0
+      ? '풍경'
+      : genre === 1
+      ? '인물'
+      : genre === 2
+      ? '동물'
+      : genre === 3
+      ? '정물'
+      : genre === 4
+      ? '추상'
+      : '기타';
+
   return (
     <div className={textStyle}>
-      <div className={styles['grade']}>RARE</div>
+      <div className={styles[gradeText.replace(/\s+/g, '-').toLowerCase()]}>
+        {gradeText}
+      </div>
       <div className={styles['bar']}>|</div>
-      <div className={styles['category']}>풍경</div>
+      <div className={styles['category']}>{genreText}</div>
     </div>
   );
 }
