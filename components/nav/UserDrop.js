8c76d0841ed5a6130154c0e5859814816d096bc0
mjpark <pmj9498@gmail.com>
2024-10-29 14:27:39 +0900

    feat: 비로그인 시 모바일 버전 유저드랍을 보여주기 참고: 디자인이나 문구는 추천해주시면 수정 가능합니다!

--- components/nav/UserDrop.js
+++ components/nav/UserDrop.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import styles from "./UserDrop.module.css";
 import { useEffect, useRef } from "react";
+import useAuthStore from "@/store/useAuthStore";
 
 export default function UserDrop({ nickname, point, setUserDrop, logout }) {
   const outRef = useRef(null);
+
+  const { user } = useAuthStore();
+
+  console.log(user);
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -22,26 +27,35 @@ export default function UserDrop({ nickname, point, setUserDrop, logout }) {
   return (
     <div className={styles["user-drop"]}>
       <div className={styles["container"]} ref={outRef}>
-        <span className={styles["title"]}>안녕하세요, {nickname}님!</span>
-        <div className={styles["point"]}>
-          <span className={styles["has-point"]}>보유 포인트</span>
-          <span className={styles["num-point"]}>
-            {point.toLocaleString("ko-KR")} P
-          </span>
-        </div>
-        <div className={styles["mobile-table"]}>
-          <div className={styles["table"]}>
-            <Link href={"/mygallery"}>
-              <span>마이갤러리</span>
-            </Link>
-            <Link href={"/mysales"}>
-              <span>나의 판매 포토카드</span>
-            </Link>
+        {user ? (
+          <>
+            <span className={styles["title"]}>안녕하세요, {nickname}님!</span>
+            <div className={styles["point"]}>
+              <span className={styles["has-point"]}>보유 포인트</span>
+              <span className={styles["num-point"]}>
+                {point.toLocaleString("ko-KR")} P
+              </span>
+            </div>
+            <div className={styles["mobile-table"]}>
+              <div className={styles["table"]}>
+                <Link href={"/mygallery"}>
+                  <span>마이갤러리</span>
+                </Link>
+                <Link href={"/mysales"}>
+                  <span>나의 판매 포토카드</span>
+                </Link>
+              </div>
+              <span className={styles["logout"]} onClick={logout}>
+                로그아웃
+              </span>
+            </div>
+          </>
+        ) : (
+          <div className={styles["title"]}>
+            안녕하세요, <br />
+            로그인을 먼저 해주세요.
           </div>
-          <span className={styles["logout"]} onClick={logout}>
-            로그아웃
-          </span>
-        </div>
+        )}
       </div>
     </div>
   );
